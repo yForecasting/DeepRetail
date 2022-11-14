8a0279f4a3ede42d5aefd23c3fdedc4446653ff7
FilTheo <theodosiou.filotas@gmail.com>
2022-11-14 12:57:15 +0200

    Updated functions for forecasting

--- DeepRetail/forecasting/extras.py
+++ DeepRetail/forecasting/extras.py
@@ -1,4 +1,6 @@
 import ray
+import numpy as np
+import pandas as pd
 
 
 @ray.remote
@@ -24,3 +26,50 @@ def for_ray(forecaster, horizon, n_windows_cv=None):
         forecast_df = forecaster.cross_validation(h=horizon, n_windows=n_windows_cv)
 
     return forecast_df
+
+
+def add_fh_cv(res_df):
+
+    # add the number of cv and fh
+    cv_vals = sorted(res_df["cutoff"].unique())
+    fh_vals = sorted(res_df["date"].unique())
+
+    cv_dict = dict(zip(cv_vals, np.arange(1, len(cv_vals) + 1)))
+    fh_dict = dict(zip(fh_vals, np.arange(1, len(fh_vals) + 1)))
+
+    res_df["fh"] = [fh_dict[date] for date in res_df["date"].values]
+    res_df["cv"] = [cv_dict[date] for date in res_df["cutoff"].values]
+
+    return res_df
+
+
+def fit_predict(model, y, y_train, fh, cross_val, name):
+
+    # Fit the model
+    model.fit(y_train, fh=fh)
+
+    # Predict
+    y_pred = model.update_predict(y, cross_val)
+
+    # Convert to the right format
+    y_pred = (
+        y_pred.unstack()
+        .unstack(level=1)
+        .reset_index()
+        .rename(columns={"level_0": "cutoff", "Period": "date"})
+    )
+
+    # Collapse
+    y_pred = pd.melt(
+        y_pred,
+        id_vars=["date", "cutoff"],
+        value_vars=y_pred.columns[2:],
+        value_name="y",
+        var_name="unique_id",
+    )
+
+    # Add the model name
+    y_pred["Model"] = name
+
+    # Return
+    return y_pred
