dfc08dddd65eafed6c5622c17160d75547a192be
FilTheo <theodosiou.filotas@gmail.com>
2023-02-15 14:57:52 +0200

    Added the structure for the Reader class

---
+++ DeepRetail/data/dataloader.py
@@ -0,0 +1,24 @@
+
+
+
+
+
+class Reader(object):
+    """
+    Reads data of different formats from the provided filepath.
+    Converts data into a universal format used throught the package.
+    
+    """
+
+    def __init__(self):
+        pass
+
+    def call_in_memory(self):
+        pass
+       
+    def save(self):
+        pass
+
+    def load(self):
+        pass
+        
