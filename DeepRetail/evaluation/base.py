b274479c7b805ae572dee546fd6268e61ab9c64c
Filotas Theodosiou <70523417+FilTheo@users.noreply.github.com>
2023-02-18 12:09:18 +0100

    Update base.py
    Added documentation to the Evaluator class

--- DeepRetail/evaluation/base.py
+++ DeepRetail/evaluation/base.py
@@ -37,7 +37,56 @@ def calculate_group_scores(group, metrics):
 
 
 class Evaluator(object):
+   """
+    Class for evaluating the performance of a forecasting model.
+
+    Args:
+        original_df (pd.DataFrame): The original input DataFrame containing the time series data.
+        result_df (pd.DataFrame): The DataFrame containing the predicted values.
+        freq (str): The frequency of the time series data, e.g. 'D' for daily, 'M' for monthly, etc.
+        format (str, optional): The format of the input DataFrame. Can be either 'pivoted' or 'transaction'. Defaults to 'pivoted'.
+
+    Methods:
+        calculate_in_sample_metrics(self):
+            Calculates the in-sample metrics for the input DataFrame.
+
+        evaluate(self, metrics, group_scores_by=["unique_id", "Model", "fh", "cv"]):
+            Evaluates the predictions and calculates the specified metrics for each group.
+
+
+    Examples:
+        >>> from DeepRetail.evaluation import Evaluator
+
+        # Load original and result dataframes
+        >>> original_df = pd.read_csv('original_data.csv')
+        >>> result_df = pd.read_csv('result_data.csv')
+
+        # Create an Evaluator object
+        >>> evaluator = Evaluator(original_df, result_df, freq='D', format='pivoted')
+
+        # Define the metrics to be evaluated
+        >>> metrics = [mse, mae]
+
+        # Group scores by unique_id, Model, fh, and cv
+        >>> group_scores_by = ['unique_id', 'Model', 'fh', 'cv']
+
+        # Calculate the evaluation metrics
+        >>> evaluation_df = evaluator.evaluate(metrics, group_scores_by)
+
+        """
+
     def __init__(self, original_df, result_df, freq, format="pivoted"):
+        """
+        Initializes the Evaluator object.
+
+        Args:
+            original_df (pd.DataFrame): The original transactional dataframe
+            result_df (pd.DataFrame): The resulting predictions dataframe
+            freq (str): The frequency of the time series data
+            format (str): The format of the input data. Default is 'pivoted'.
+        
+        """
+        
         self.result_df = result_df
         self.freq = freq
 
@@ -50,11 +99,20 @@ class Evaluator(object):
             original_df["unique_id"].isin(result_df["unique_id"].unique())
         ]
 
+
         # Get the fh and the cv
         self.total_cv = len(self.result_df["cv"].unique())
         self.total_fh = len(self.result_df["fh"].unique())
 
     def calculate_in_sample_metrics(self):
+        """
+        Calculates the in-sample metrics(mae, mse) for the time series data.
+        Its used for estimating RMSSE and MASE.
+
+        Returns:
+            pd.DataFrame: A dataframe containing the in-sample metrics.
+        """
+
         # Creates a copy
         in_sample_metrics = pivoted_df(self.original_df, self.freq)
 
@@ -73,6 +131,22 @@ class Evaluator(object):
         return in_sample_metrics.reset_index()
 
     def evaluate(self, metrics, group_scores_by=["unique_id", "Model", "fh", "cv"]):
+        """
+        Evaluates the predictions and calculates the specified metrics for each group.
+
+        Args:
+            metrics (list): A list of metrics to be calculated for each group. Each metric should be a function that
+                            takes two arguments: a NumPy array of true values and a NumPy array of predicted values. The
+                            list of metrics can also include optional arguments to be passed to the metric function.
+            group_scores_by (list): A list of columns to group the predictions by. The default value groups by unique_id,
+                                    Model, fh, and cv.
+
+        Returns:
+            pd.DataFrame: A DataFrame containing the scores for each group, with each row representing a single group and
+                        each column representing a metric. The DataFrame also contains the columns used for grouping
+                        the predictions.
+
+        """
         # Estimate the insample naive mse and mae
         # They are used for scalling
         in_sample_metrics = self.calculate_in_sample_metrics()
