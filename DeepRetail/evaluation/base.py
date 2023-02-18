21e39d9fc27d2ca72a0d2c8d54e93a5b750edd9d
Filotas Theodosiou <70523417+FilTheo@users.noreply.github.com>
2023-02-18 12:12:15 +0100

    Update base.py
    Fixed some issues with the format of the documentation.

--- DeepRetail/evaluation/base.py
+++ DeepRetail/evaluation/base.py
@@ -37,14 +37,15 @@ def calculate_group_scores(group, metrics):
 
 
 class Evaluator(object):
-   """
+    """
     Class for evaluating the performance of a forecasting model.
 
     Args:
         original_df (pd.DataFrame): The original input DataFrame containing the time series data.
         result_df (pd.DataFrame): The DataFrame containing the predicted values.
         freq (str): The frequency of the time series data, e.g. 'D' for daily, 'M' for monthly, etc.
-        format (str, optional): The format of the input DataFrame. Can be either 'pivoted' or 'transaction'. Defaults to 'pivoted'.
+        format (str, optional): The format of the input DataFrame. Can be either 'pivoted' or 'transaction'.
+            Defaults to 'pivoted'.
 
     Methods:
         calculate_in_sample_metrics(self):
@@ -84,9 +85,9 @@ class Evaluator(object):
             result_df (pd.DataFrame): The resulting predictions dataframe
             freq (str): The frequency of the time series data
             format (str): The format of the input data. Default is 'pivoted'.
-        
+
         """
-        
+
         self.result_df = result_df
         self.freq = freq
 
@@ -98,7 +99,6 @@ class Evaluator(object):
         self.original_df = original_df[
             original_df["unique_id"].isin(result_df["unique_id"].unique())
         ]
-
 
         # Get the fh and the cv
         self.total_cv = len(self.result_df["cv"].unique())
@@ -138,14 +138,11 @@ class Evaluator(object):
             metrics (list): A list of metrics to be calculated for each group. Each metric should be a function that
                             takes two arguments: a NumPy array of true values and a NumPy array of predicted values. The
                             list of metrics can also include optional arguments to be passed to the metric function.
-            group_scores_by (list): A list of columns to group the predictions by. The default value groups by unique_id,
-                                    Model, fh, and cv.
+            group_scores_by (list): A list of columns to group the predictions by.
+                The default value groups by unique_id, Model, fh, and cv.
 
         Returns:
-            pd.DataFrame: A DataFrame containing the scores for each group, with each row representing a single group and
-                        each column representing a metric. The DataFrame also contains the columns used for grouping
-                        the predictions.
-
+            pd.DataFrame: A DataFrame containing the scores for each group
         """
         # Estimate the insample naive mse and mae
         # They are used for scalling
