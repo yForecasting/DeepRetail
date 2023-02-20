d6c5231b204a0c3ea3ab33d77572c8fea2e32541
Filotas Theodosiou <70523417+FilTheo@users.noreply.github.com>
2023-02-20 08:56:55 +0100

    Added the visualize_forecasts functions.
    Plots multiple time series along with forecasts from the defined models.

--- DeepRetail/visuals/evaluation.py
+++ DeepRetail/visuals/evaluation.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+from DeepRetail.transformations.formats import pivoted_df
 
 
 def plot_single_hist_boxplot(eval_df_temp, metrics, model):
@@ -185,3 +186,86 @@ def plot_line(evaluation_df, metrics):
 
     # plt.tight_layout()
     plt.show()
+
+
+def visualize_forecasts(n, h, cv, freq, original_df, forecast_df, models, show_in_sample):
+    """
+    Visualize the forecasts for the given models.
+    If defined, plots the in-samples values too.
+
+    Args:
+        n (int): The number of series to plot.
+        h (int): The forecast horizon.
+        freq (str): The frequency of the data.
+        original_df (pd.DataFrame): The original dataframe.
+        forecast_df (pd.DataFrame): The forecast dataframe.
+        models (list): The models to consider.
+        show_in_sample (bool): Whether to show the in-sample values or not.
+    """
+
+    # Initialize parameters
+    dates = pd.to_datetime(original_df.columns)
+    titles = original_df.index.astype(str)
+    to_mask = len(dates) - h
+    end_date = dates[-h]
+
+    # Initialize figure specific parameters
+    row_to_vis = n if n < 3 else 3
+    total_to_vis = n - n % row_to_vis
+    gray_scale = 0.9
+
+    # Run loop
+    for idx in range(0, total_to_vis, row_to_vis):
+        plt.figure(figsize=(20, 15))
+        for i in range(0, row_to_vis):
+            ax = plt.subplot(row_to_vis, row_to_vis, i + 1)
+
+            # the series index to plot
+            series_index = idx + i
+            series_name = titles[series_index]
+            y = original_df.loc[series_name]
+
+            if show_in_sample:
+                # plot the in_sample values
+                y.plot(ax=ax, label='y')
+                # Line to seperate train/test
+                plt.axvline(x=end_date, color="black", linestyle="--")
+                plt.axvspan(xmin=end_date, xmax=dates[-1], color="darkgray")
+
+            # Plot predictions
+            for model in models:
+                # Filter values
+                temp_fc_df = forecast_df[forecast_df['Model'] == model]
+
+                # Currently only plots for the last cv!
+                temp_fc_df_fold = temp_fc_df[temp_fc_df['cv'] == cv]
+                # pivot the values and take the series to show
+                temp_vals = pivoted_df(temp_fc_df_fold, freq).loc[series_name].values
+
+                # if to show in-sample
+                if show_in_sample:
+                    # pad & convert to series for plotting
+                    temp_vals = np.pad(
+                        temp_vals, pad_width=(to_mask, 0), constant_values=None
+                    )  # we use rolling cv so we have to increase the to pad every time
+
+                    temp_vals = pd.Series(temp_vals, index=dates)
+
+                    # Plot
+                    temp_vals.plot(ax=ax, label=model)
+
+                else:
+                    # plot without the mask
+                    temp_vals.plot(ax=ax, label=model)
+
+            # Title:
+            # currently not supporting the metric on title
+            title = series_name
+            plt.title(title)
+
+            # Edit the format of the plot
+            plt.xticks(rotation=45)
+            plt.grid()
+            plt.legend()
+
+            ax.set_facecolor((gray_scale, gray_scale, gray_scale))
