5db9ed0ec5db90cca12b050f2c5c3f3c98ea59ea
FilTheo <theodosiou.filotas@gmail.com>
2023-10-23 17:23:58 +0300

    fixed a typo

--- DeepRetail/exploratory/eda.py
+++ DeepRetail/exploratory/eda.py
@@ -23,7 +23,7 @@ def moving_average(a, n=3):
     """
     ret = np.cumsum(a, dtype=float)
     ret[n:] = ret[n:] - ret[:-n]
-    return ret[n - 1 :] / n
+    return ret[n - 1 :] / n  # noqa: E203
 
 
 def visualize_series(
